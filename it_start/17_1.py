883428c79053653edcd72f119b11a9b686d9a043
Ratata <84903507+PY079@users.noreply.github.com>
2023-11-17 22:56:43 +0300

    Delete it_start directory

--- it_start/17_1.py
+++
@@ -1,5 +0,0 @@
-# можно генерировать любые числа, но не меня число в for
-from random import randint
-arr = [randint(1,500) for _ in range(100)]
-a = int(input('Введите число от 1 до 500: '))
-print([number for number in arr if number<a])
