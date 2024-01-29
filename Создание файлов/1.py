6600a6bc7f19e8a330e8e1c2a830e0e4a2ce10d1
Ratata <84903507+PY079@users.noreply.github.com>
2024-01-29 13:06:48 +0300

    Create 1.py

---
+++ Создание файлов/1.py
@@ -0,0 +1,21 @@
+import os, pymorphy2
+path_file='\\'.join(os.path.abspath(__file__).split("\\")[:-1])
+
+def write_file_name():
+    with open(fr'{path_file}\file.txt','+w') as file:
+        for _ in range(10_000):
+            file.write('ТВОЕ ФИО\n')
+
+def read_file():
+    with open(fr'{path_file}\file.txt','r') as file:
+        text=file.readlines()
+        for fio in text:
+            fio_split=fio.rstrip().split(' ')
+            last_name=fio_split[0]
+            name=fio_split[1]
+            otch=fio_split[2]
+
+            morph1 = pymorphy2.MorphAnalyzer().parse(last_name)[0].inflect({'gent'}).word
+            morph2 = pymorphy2.MorphAnalyzer().parse(name)[0].inflect({'gent'}).word
+            morph3 = pymorphy2.MorphAnalyzer().parse(otch)[0].inflect({'gent'}).word
+            print(morph1.title(),morph2.title(),morph3.title())
