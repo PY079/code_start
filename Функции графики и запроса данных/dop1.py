0c6d30d6c84fbf646d12a56d44708209e15e40a8
Ratata <84903507+PY079@users.noreply.github.com>
2024-01-29 12:51:13 +0300

    Create dop1.py

---
+++ Функции графики и запроса данных/dop1.py
@@ -0,0 +1,15 @@
+import turtle, os
+
+def box():
+    obj=turtle.Turtle()
+    obj.speed(5)
+    for _ in range(4):
+        obj.forward(90)
+        obj.left(90)
+    turtle.done()
+
+
+def get_data():
+    fio,email,phone_number=input('Введите фио '), input('Введите почту: '), input('Введите номер телефона: ')
+    os.system('cls')
+    return f'Ваши данные\n\nФИО: {fio}; Почта: {email}; Номер телефона: {phone_number}'
